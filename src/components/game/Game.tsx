3922a772e48a4f09e63692f84520577183be3f16
vetapteka <cvetka_top@mail.ru>
2023-03-02 23:23:44 +0300

    add base functionality for Game

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,5 +1,38 @@
+import { useRef } from 'react';
+import Tile, { TileRef } from './tile/Tile';
+
+const x = 16;
+const y = 16;
+
 const Game = () => {
-    return <div></div>;
+    const tileRefs = useRef<Array<TileRef | null>>([]);
+
+    const createTiles = () => {
+        const tiles = [];
+        for (let i: number = 0; i < y * x; i++) {
+            tiles.push(
+                <Tile
+                    key={i}
+                    ref={(el) => (tileRefs.current[i] = el)}
+                    onClick={handleTileClick}
+                    index={i}
+                />
+            );
+        }
+        return tiles;
+    };
+
+    const handleTileClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        const target = event.target as HTMLDivElement;
+        const index = target.dataset.index;
+        console.log(index);
+
+        if (typeof index !== 'undefined') {
+            tileRefs.current[+index]?.handleOpen();
+        }
+    };
+
+    return <>{createTiles()}</>;
 };
 
 export default Game;
